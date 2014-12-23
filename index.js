c5f33a4a285a02b8bfa895d0040e57a5a941a881
jessxlee <jessicalee2010@gmail.com>
2014-12-23 06:52:11 +0000

    Updated index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express')
 var app = express();
-var text = fs.readFileSync('index.html')
+var buffer = fs.readFileSync('index.html');
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(text)
+  response.send(buffer.toString())
 })
 
 app.listen(app.get('port'), function() {
